1fe36c41928c9861774d351a45cb7f74a7513c63
LolPaGo <lolpago@example.com>
2026-03-30 14:12:02 +0900

    add Google AdSense meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="google-site-verification" content="hsjncRi9cl3tz3Otd6SJKurSt_V1bZ0AKO-bdWIGeHM" />
+        <meta name="google-adsense-account" content="ca-pub-4361110443201092" />
         <meta name="keywords" content="currency converter, exchange rate, USD, EUR, GBP, currency calculator, forex" />
         <script
           async
